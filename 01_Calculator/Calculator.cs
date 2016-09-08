80fd8521c2f3c2c481c4b4895bdfb68b90385f1a
Malene <male8642@edu.eal.dk>
2016-09-08 09:54:25 +0200

    fixed code

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    internal class Calculator
+    {
+        internal static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        internal static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        internal static int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        internal static int Sum(int[] numbers)
+        {
+            int allSum = 0;
+            
+            for (int i = 0; i<numbers.Length; i++)
+
+            {
+                allSum = allSum + numbers[i];
+            }
+
+            return allSum;
+
+        }
+
+        internal static object Multiply(int v1, int v2, int v3)
+        {
+            return v1 * v2 * v3;
+        }
+
+        internal static double Power(int v1, int v2)
+        {
+            return Math.Pow(v1, v2);
+        }
+
+        internal static int Factorial(int v)
+        {
+            int result = 1;
+
+            for (int i = 2; i <= v; i++)
+
+            {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
--- 01_Calculator/CalculatorTest.cs
+++ 01_Calculator/CalculatorTest.cs
@@ -38,24 +38,34 @@ namespace _01_Calculator
         {
             //It multiplies two numbers
             //It multiplies several numbers
-            throw new NotImplementedException();
+            Assert.AreEqual(0, Calculator.Multiply(0, 0));
+            Assert.AreEqual(8, Calculator.Multiply(2, 4));
+            Assert.AreEqual(-12, Calculator.Multiply(2, -6));
+            Assert.AreEqual(12, Calculator.Multiply(-2, -6));
+            Assert.AreEqual(20, Calculator.Multiply(2, 5, 2));
+
         }
         [TestMethod]
         public void Power()
         {
             //It raises one number to the power of another number
-            throw new NotImplementedException();
+            Assert.AreEqual(1, Calculator.Power(2, 0));
+            Assert.AreEqual(2, Calculator.Power(2, 1));
+            Assert.AreEqual(64, Calculator.Power(4, 3));
         }
         [TestMethod]
         public void Factorial()
         {
             //# http://en.wikipedia.org/wiki/Factorial
             //  it "computes the factorial of 0"
-            //  it "computes the factorial of 1"
-            //  it "computes the factorial of 2"
-            //  it "computes the factorial of 5"
-            //  it "computes the factorial of 10"
-            throw new NotImplementedException();
+            //it "computes the factorial of 1"
+            //it "computes the factorial of 2"
+            //it "computes the factorial of 5"
+            //it "computes the factorial of 10"
+
+            Assert.AreEqual(120, Calculator.Factorial(5));
+            Assert.AreEqual(5040, Calculator.Factorial(7));
         }
     }
 }
+
