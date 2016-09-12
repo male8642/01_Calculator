36a34963e5366e53a6ea6fb602f65dd3ac1a4752
Malene <male8642@edu.eal.dk>
2016-09-12 19:19:35 +0200

    attempt at calculator app

--- 01_Calculator/Calculator.cs
+++ 01_Calculator/Calculator.cs
@@ -2,24 +2,24 @@
 
 namespace _01_Calculator
 {
-    internal class Calculator
+    public class Calculator
     {
-        internal static int Add(int v1, int v2)
+        public static int Add(int v1, int v2)
         {
             return v1 + v2;
         }
 
-        internal static int Subtract(int v1, int v2)
+        public static int Subtract(int v1, int v2)
         {
             return v1 - v2;
         }
 
-        internal static int Multiply(int v1, int v2)
+        public static int Multiply(int v1, int v2)
         {
             return v1 * v2;
         }
 
-        internal static int Sum(int[] numbers)
+        public static int Sum(int[] numbers)
         {
             int allSum = 0;
             
@@ -33,17 +33,17 @@ namespace _01_Calculator
 
         }
 
-        internal static object Multiply(int v1, int v2, int v3)
+        public static object Multiply(int v1, int v2, int v3)
         {
             return v1 * v2 * v3;
         }
 
-        internal static double Power(int v1, int v2)
+        public static double Power(int v1, int v2)
         {
             return Math.Pow(v1, v2);
         }
 
-        internal static int Factorial(int v)
+        public static int Factorial(int v)
         {
             int result = 1;
 
